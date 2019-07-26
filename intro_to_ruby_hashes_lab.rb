86915d1ea2d83a7cb54bfb36fb2f88d97cca7216
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-26 18:49:15 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,13 +3,11 @@ def new_hash
 end
 
 def actor
-  {
-    actor = {
-      :name = "Dwayne The Rock Johnson"
+  actor = {
+    :name = "Dwayne The Rock Johnson"
     }
-  }
 end
-end
+
 def monopoly
 	monopoly = {}
 
