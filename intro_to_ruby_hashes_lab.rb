dfbc18505a0c41507ac9c0e1c26c23dec672501a
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-26 18:59:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,8 +13,7 @@ def monopoly
 	  rent_in_dollars: = {}
 	}}
 end
-end
-end
+
 def monopoly_with_second_tier
 	monopoly = {railroads: {}}
 	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
